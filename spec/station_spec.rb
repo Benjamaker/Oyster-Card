7bcd6372558d77689f86e24ee84a6644d39c3ee3
Ben Galley <bengalley21@gmail.com>
2020-02-26 14:56:48 +0000

    changed tests for Station class to check for these values

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,8 +1,15 @@
 require 'station'
 
 describe Station do
-  it 'checks the station has name and zone attributes' do
-    expect(subject).to have_attributes(name: nil, zone: nil)
+
+subject { described_class.new("old street", 1) }
+
+  it "knows its name" do
+    expect(subject.name).to eq("old street")
+  end
+
+  it "knows it's zone" do
+    expect(subject.zone).to eq(1)
   end
 
 
