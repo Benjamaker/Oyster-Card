41eb73b1df9fafd0d5e1cbcb530bbed785f68df5
Ben Galley <bengalley21@gmail.com>
2020-02-27 10:05:56 +0000

    added a second double to separate entry and exit stations in tests

--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -5,7 +5,8 @@ describe Journey do
 
   # let(:stn_dbl) { double :station, zone: 1 }
 
-  let(:stn) { double(name: "liverpool st", zone: 1) }
+  let(:entry_stn) { double(name: "liverpool st", zone: 1) }
+  let(:exit_stn) { double(name: "tottenham court road", zone: 1) }
 
   it "has attributes entry_station and exit_station" do
     expect(subject).to have_attributes(entry_station: nil , exit_station: nil)
@@ -22,7 +23,7 @@ describe Journey do
     end
 
     it "updates entry station when start_journey method is called" do
-      subject.start_journey(stn)
+      subject.start_journey(entry_stn)
       expect(subject.entry_station).to eq "liverpool st"
     end
 
@@ -35,8 +36,8 @@ describe Journey do
     end
 
     it "updates exit station when end_journey method is called" do
-      subject.end_journey(stn)
-      expect(subject.exit_station).to eq "liverpool st"
+      subject.end_journey(exit_stn)
+      expect(subject.exit_station).to eq "tottenham court road"
     end
 
 
@@ -48,6 +49,12 @@ describe Journey do
       expect(subject).to respond_to(:fare)
     end
   end
+
+    it "charges the minimum fare when journey is completed" do
+      subject.start_journey(entry_stn)
+      subject.end_journey(exit_stn)
+      expect(subject.fare).to eq Journey::MIN_FARE
+    end
 
   context '#complete?' do
     it "responds to a method complete?" do
